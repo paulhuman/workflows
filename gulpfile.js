66194fa811349512ff602fb975d22d841789f7cb
Paul Human <hellopaulhuman@gmail.com>
2017-02-07 18:14:41 +0300

    Concatenated JavaScript Files

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,26 @@
 
 var gulp   = require('gulp'),
     util   = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'
+];
 
 gulp.task('coffee', function () {
     gulp.src(coffeeSources)
-        .pipe(coffee({
-            bare: true
-        }).on('error', util.log))
-        .pipe(gulp.dest('components/scripts'))
+        .pipe(coffee({ bare: true }).on('error', util.log))
+        .pipe(gulp.dest('components/scripts'));
 
-})
+});
+
+gulp.task('concat', function () {
+    gulp.src(jsSources)
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('builds/development/js'));
+});
